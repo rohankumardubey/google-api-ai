568e4328d6d72eca4ea9d763e035467b35c34607
Deepanshu <Deepanshu.gupta@clairvoyantsoft.com>
2017-03-03 20:29:41 +0530

    loging

--- application-build-assistant/src/main/java/com/build/assistant/rest/BaseController.java
+++ application-build-assistant/src/main/java/com/build/assistant/rest/BaseController.java
@@ -3,42 +3,58 @@ package com.build.assistant.rest;
 import com.build.assistant.domain.Data;
 import com.build.assistant.domain.Request;
 import com.build.assistant.domain.Response;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api")
 public class BaseController {
+    final org.slf4j.Logger log = LoggerFactory.getLogger(BaseController.class);
 
     /* Deploy the application */
     @RequestMapping(value = "/deploy", method = RequestMethod.POST)
     @ResponseBody
     public Response deployApplication(@RequestBody Request request){
         Response response = new Response();
-        if(request != null && request.getResult() != null && request.getResult().getAction().equals("application.deploy")){
 
-            response.setSpeech(" Successfully deployed ");
-            response.setDisplayText("Display Successfully Deployed");
-            Data data = new Data();
-            data.setAdditionalProperty("Depployed", request.getResult().getAction().toString());
-            response.setData(data);
-            List<Object> context = new ArrayList<>();
-            context.add("deploycontext");
-            response.setContextOut(context);
-        }else if(request != null && request.getResult() != null && request.getResult().getAction().equals("getBranches")){
-            response.setSpeech(" List of Branches is Dev, Master ");
-            response.setDisplayText("Got brances");
-            Data data = new Data();
-            data.setAdditionalProperty("Branch list", request.getResult().getAction().toString());
-            response.setData(data);
-            List<Object> context = new ArrayList<>();
-            context.add("deploycontext");
-            response.setContextOut(context);
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            System.out.println("Request incoming " + mapper.writeValueAsString(request));
+            log.info("Recieved Request 1");
+            log.info("Recieved Request 1 Action: " + request.getResult().getAction().toString());
+            if (request != null && request.getResult() != null && request.getResult().getAction().equals("deploy")) {
+
+                response.setSpeech(" Successfully deployed ");
+                response.setDisplayText("Display Successfully Deployed");
+                Data data = new Data();
+                data.setAdditionalProperty("Depployed", request.getResult().getAction().toString());
+                response.setData(data);
+                List<Object> context = new ArrayList<>();
+                context.add("deploycontext");
+                response.setContextOut(context);
+            } else if (request != null && request.getResult() != null && request.getResult().getAction().equals("getBranches")) {
+                response.setSpeech(" List of Branches is Dev, Master ");
+                response.setDisplayText("Got brances");
+                Data data = new Data();
+                data.setAdditionalProperty("Branch list", request.getResult().getAction().toString());
+                response.setData(data);
+                List<Object> context = new ArrayList<>();
+                context.add("deploycontext");
+                response.setContextOut(context);
+            }
+        }catch (Exception e){
+            log.error(e.getMessage());
+            e.printStackTrace();
         }
+
+        log.info("Sending Response");
 
         return response;
     }
