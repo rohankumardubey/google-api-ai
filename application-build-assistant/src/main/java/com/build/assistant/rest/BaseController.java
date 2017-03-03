3d1c7ea7e4249b7c56df426d33a6c403f4b5e33e
Amit Kulkarni <amit.kulkarni@clairvoyantsoft.com>
2017-03-03 17:47:23 +0530

    Basic SpringBoot App

---
+++ application-build-assistant/src/main/java/com/build/assistant/rest/BaseController.java
@@ -0,0 +1,43 @@
+package com.build.assistant.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class BaseController {
+
+    /* Deploy the application */
+    @RequestMapping(value = "/deploy", method = RequestMethod.POST)
+    public String deployApplication(){
+        return "success";
+    }
+
+    /* Get the list of available branches */
+    @RequestMapping(value = "/branch", method = RequestMethod.GET)
+    public List<String> getRelatedBraches(){
+        List<String> availableBranches = new ArrayList<>();
+        availableBranches.add("SPRINT-13_New_Feature_Dev");
+        availableBranches.add("SPRINT-14_New_Feature_Enhance");
+        availableBranches.add("SPRINT-13_New_Feature_Release");
+        availableBranches.add("development");
+        availableBranches.add("master");
+
+        return availableBranches;
+    }
+
+    /* Get the list of available environments */
+    @RequestMapping(value = "/environment", method = RequestMethod.GET)
+    public List<String> getEnvironment(){
+        List<String> availableEnvironments = new ArrayList<>();
+        availableEnvironments.add("Dev");
+        availableEnvironments.add("QA");
+        availableEnvironments.add("Prod");
+
+        return availableEnvironments;
+    }
+}
