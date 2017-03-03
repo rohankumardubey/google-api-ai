6ec70a54fade3973dc33923569156c2b9a5da154
Deepanshu <Deepanshu.gupta@clairvoyantsoft.com>
2017-03-03 19:04:22 +0530

    api

--- application-build-assistant/src/main/java/com/build/assistant/rest/BaseController.java
+++ application-build-assistant/src/main/java/com/build/assistant/rest/BaseController.java
@@ -1,8 +1,8 @@
 package com.build.assistant.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.build.assistant.domain.Request;
+import com.build.assistant.domain.Response;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +13,20 @@ public class BaseController {
 
     /* Deploy the application */
     @RequestMapping(value = "/deploy", method = RequestMethod.POST)
-    public String deployApplication(){
-        return "success";
+    @ResponseBody
+    public Response deployApplication(@RequestBody Request request){
+        Response response = new Response();
+        if(request != null && request.getResult() != null && request.getResult().getAction().equals("application.deploy")){
+
+            response.setSpeech(" Successfully deployed ");
+        }else if(request != null && request.getResult() != null && request.getResult().getAction().equals("getBranches")){
+            response.setSpeech(" List of Branches is Dev, Master ");
+        }
+        return response;
     }
 
     /* Get the list of available branches */
-    @RequestMapping(value = "/branch", method = RequestMethod.GET)
+    /*@RequestMapping(value = "/branch", method = RequestMethod.GET)
     public List<String> getRelatedBraches(){
         List<String> availableBranches = new ArrayList<>();
         availableBranches.add("SPRINT-13_New_Feature_Dev");
@@ -30,7 +38,7 @@ public class BaseController {
         return availableBranches;
     }
 
-    /* Get the list of available environments */
+    *//* Get the list of available environments *//*
     @RequestMapping(value = "/environment", method = RequestMethod.GET)
     public List<String> getEnvironment(){
         List<String> availableEnvironments = new ArrayList<>();
@@ -39,5 +47,5 @@ public class BaseController {
         availableEnvironments.add("Prod");
 
         return availableEnvironments;
-    }
+    }*/
 }
